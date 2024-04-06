3b00f3e29669303051632d83ae41089500281518
Andrew Parra <nixonandrewparra@gmail.com>
2024-04-06 11:12:10 -0500

    Mensaje de commit descriptivo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { data } from "./Data";
+import Carousel from "./components/Carousel";
+import Contactame from "./components/Contactame";
+import Footer from "./components/Footer";
+import NavHeader from "./components/NavHeader";
+import Proyectos from "./components/Proyectos";
+import SobreMi from "./components/SobreMi";
+
+function App() {
+  return (
+    <>
+      {/* inicio de menu */}
+      <header>
+        <NavHeader />
+      </header>
+      {/* fin del menu */}
+
+      {/* contenido principal */}
+      <main className="grid gap-10   justify-center items-center">
+        <SobreMi />
+        <Proyectos />
+        <>
+          <Carousel slides={data} />
+        </>
+        <Contactame />
+
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+export default App;
